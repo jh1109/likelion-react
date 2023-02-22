c393f7f92e149565c7158af726ed8b1c33bffcb7
Juhee Noh <nohjuhee227@gmail.com>
2023-02-22 11:00:23 +0900

    [learn] React.useEffect()

--- counter-with-cra/src/app/App.jsx
+++ counter-with-cra/src/app/App.jsx
@@ -7,6 +7,8 @@ import {
   LifeCycle,
 } from '../components';
 
+import { API_ENDPOINT } from '../components/LifeCycle/LifeCycle';
+
 function renderComponents(isVisible) {
   if (isVisible) {
     return (
@@ -25,6 +27,21 @@ function renderComponents(isVisible) {
 }
 
 function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  const [data, setData] = React.useState('');
+  // 여기서 data는 문자라고 가정
+
+  // 사이드 이펙트 처리하는 hook 활용해보자!
+  React.useEffect(()=>{
+    console.log('componentDidMount');
+  }, [])
+  // 여기서 []빈 배열을 넣지 않는다면 상태가 업데이트 될 때 마다 계속해서 리 렌더링이 됨
+  // 계속된다면 성능 부분에서 저하될 수 있음. 그래서 기본으로 빈 배열을 넣어줌!!
+
+
+
+  
   // 상태 변수(state variable)
   const [isVisibleComponents, updateIsVisibleComponents] = React.useState(false);
 
